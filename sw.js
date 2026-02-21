b3d19c363d2310d7b8592b22c4b6b542ca40a29e
RubenZ21 <ruben.zubac@gmail.com>
2026-02-21 17:34:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE = 'r1-v1';
+const CACHE = 'r1-v5';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html', './manifest.json'])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html', './manifest.json', './icon-192.png', './icon-512.png'])));
   self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
