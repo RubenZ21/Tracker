b6c3f1ad168a7ecc9d1108cb5bcbc18a3c923bfc
RubenZ21 <ruben.zubac@gmail.com>
2026-02-25 14:48:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', e => {
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
